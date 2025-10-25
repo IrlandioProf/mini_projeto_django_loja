e149bc5283d158aa317849ea42dc6b3c2b711ff8
Professor <Professor@SN-345321.edu.fiesc.com.br>
2025-10-25 13:20:38 -0300

    ajustes de funcionalidades

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from .models import Cliente
 
 class ClienteList(ListView):
@@ -23,3 +23,19 @@ class ClienteDelete(DeleteView):
     model = Cliente
     success_url = reverse_lazy('clientes:lista')
     template_name = 'clientes/confirma_exclusao.html'
+
+
+class HomeView(TemplateView):
+    """Página inicial que mostra os últimos clientes adicionados.
+
+    Usa TemplateView e injeta `ultimos_clientes` no contexto com os 5
+    clientes mais recentes (ordenados por id decrescente).
+    """
+    template_name = 'clientes/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Importa localmente para evitar problemas de import cycle em tempo de carregamento
+        from .models import Cliente
+        context['ultimos_clientes'] = Cliente.objects.order_by('-id')[:5]
+        return context
--- projeto_loja/urls.py
+++ projeto_loja/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.clientes.views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("clientes/", include("apps.clientes.urls")),
     path("api/", include("apps.clientes.api_urls")),
-    path("", include("apps.clientes.urls")),  # home provisória
+    # Home: renderiza template simples em vez de redirecionar
+    path("", HomeView.as_view(), name='home'),
 ]
